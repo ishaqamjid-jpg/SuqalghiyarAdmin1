45131159d162d7eef88b14c717e38874e8a3f7a5
ishaqamjid-jpg <ishaq.amjid@gmail.com>
2026-05-27 19:28:09 +0300

    AdminRepository.kt

---
+++ app/src/main/java/com/isaac/suqalghiyaradmin/AdminRepository.kt
@@ -0,0 +1,7 @@
+
+package com.isaac.souqalghiyaradmin.domain.repository
+
+interface AdminRepository {
+    // دالة تسجيل الدخول تبحث في Firebase وترجع إما الصلاحية (Access) أو null في حال الفشل
+    suspend fun loginAdmin(username: String, password: String): String?
+}
