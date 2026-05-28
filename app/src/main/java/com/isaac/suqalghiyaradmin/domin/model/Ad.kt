171fab5190d937a29b071aff3855fc7dc4f396b1
ishaqamjid-jpg <ishaq.amjid@gmail.com>
2026-05-28 21:23:17 +0300

    Update Ad.kt

--- app/src/main/java/com/isaac/suqalghiyaradmin/domin/model/Ad.kt
+++ app/src/main/java/com/isaac/suqalghiyaradmin/domin/model/Ad.kt
@@ -3,6 +3,13 @@ package com.isaac.souqalghiyaradmin.domain.model
 data class Ad(
     val id: String = "",
     val title: String = "",
-    val description: String = "",
-    val imageUrl: String = ""
+    val description: String = "", // تأكد من إضافة هذا الحقل إذا كنت تستخدمه في الواجهة
+    val image_url: String = "",
+    val click_action_type: String = "",
+    val target_url: String = "",
+    val priority: Int = 0,
+    val is_active: Boolean = false,
+    val start_date: com.google.firebase.Timestamp? = null,
+    val end_date: com.google.firebase.Timestamp? = null,
+    val created_at: com.google.firebase.Timestamp? = null
 )
