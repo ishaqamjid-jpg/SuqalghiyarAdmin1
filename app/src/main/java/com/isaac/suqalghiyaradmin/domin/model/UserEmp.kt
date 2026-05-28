5c3596cf77ce0390cbab26d01c96a3ebd5a8a14b
ishaqamjid-jpg <ishaq.amjid@gmail.com>
2026-05-28 21:25:14 +0300

    Update UserEmp.kt

--- app/src/main/java/com/isaac/suqalghiyaradmin/domin/model/UserEmp.kt
+++ app/src/main/java/com/isaac/suqalghiyaradmin/domin/model/UserEmp.kt
@@ -1,9 +1,9 @@
 package com.isaac.souqalghiyaradmin.domain.model
 
 data class UserEmp(
-    val id: String = "",       // هذا سيكون مخزناً فيه الـ Document ID بعد جلبه من Firebase
-    val username: String = "", 
-    val password: String = "", 
-    val name: String = "",     
-    val access: String = "employ"
+    val id: String = "",
+    val name: String = "",
+    val username: String = "",
+    val password: String = "",
+    val access: String = ""
 )
