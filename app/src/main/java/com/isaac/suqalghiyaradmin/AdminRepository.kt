14332e9c6f2305358674ecc1cee1a128768136b4
ishaqamjid-jpg <ishaq.amjid@gmail.com>
2026-05-28 18:34:44 +0300

    Delete AdminRepository.kt

--- app/src/main/java/com/isaac/suqalghiyaradmin/AdminRepository.kt
+++
@@ -1,7 +0,0 @@
-
-package com.isaac.souqalghiyaradmin.domain.repository
-
-interface AdminRepository {
-    // دالة تسجيل الدخول تبحث في Firebase وترجع إما الصلاحية (Access) أو null في حال الفشل
-    suspend fun loginAdmin(username: String, password: String): String?
-}
