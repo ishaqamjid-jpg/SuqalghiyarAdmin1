6b843c6e0f2c7b4b3a2426e9348a95496c49d3e0
ishaqamjid-jpg <ishaq.amjid@gmail.com>
2026-05-28 20:46:40 +0300

    Update ConstantsViewModel.kt

--- app/src/main/java/com/isaac/suqalghiyaradmin/presentation/constants/ConstantsViewModel.kt
+++ app/src/main/java/com/isaac/suqalghiyaradmin/presentation/constants/ConstantsViewModel.kt
@@ -1,1 +1,20 @@
+package com.isaac.souqalghiyaradmin.presentation.constants
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.isaac.souqalghiyaradmin.domain.repository.ConstantsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class ConstantsViewModel @Inject constructor(repository: ConstantsRepository) : ViewModel() {
+
+    val categories = repository.getSparePartCategories()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
+
+    val qualities = repository.getQualityTypes()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
+}
 
