16798f70a7efd88ed442b278584667013ae772bd
ishaqamjid-jpg <ishaq.amjid@gmail.com>
2026-05-28 18:38:23 +0300

    Delete UserEmp.kt

--- app/src/main/java/com/isaac/suqalghiyaradmin/UserEmp.kt
+++
@@ -1,9 +0,0 @@
-package com.isaac.souqalghiyaradmin.domain.model
-
-data class UserEmp(
-    val id: String = "",       // هذا سيكون مخزناً فيه الـ Document ID بعد جلبه من Firebase
-    val username: String = "", 
-    val password: String = "", 
-    val name: String = "",     
-    val access: String = "employ"
-)
